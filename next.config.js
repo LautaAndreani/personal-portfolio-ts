891e2831795301147f64a0adb9a3509b6acd3988
LautaAndreani <lautaroandreani2016@gmail.com>
2023-02-06 20:34:31 -0300

    fix: redirects to resume

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'cv.lautaroandreani.com',
+          },
+        ],
+        permanent: true,
+        destination: '/resume.pdf',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
