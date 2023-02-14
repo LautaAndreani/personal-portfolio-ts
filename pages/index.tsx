cbb4bceeb481a8bc24144e55276f9987dc701c13
LautaAndreani <lautaroandreani2016@gmail.com>
2023-02-14 12:16:41 -0300

    feat: command palette
    feat:keys event listener

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,31 @@ import FrequentlyTechs from '@/components/FrequentlyTechs'
 import LastProjects from '@/components/LastProjects'
 import Nav from '@/components/Nav'
 import { Repositories } from '@/models/models'
+import { useEffect, useState } from 'react'
 
 type Props = {
   data: Repositories[]
 }
 
 export default function Home({ data }: Props) {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  useEffect(() => {
+    document.addEventListener('keydown', (e) => {
+      e.preventDefault()
+      if(e.ctrlKey && e.key === 'p') {
+        setToggleMenu(!toggleMenu)
+      }
+     })
+
+     return () => {
+      document.removeEventListener('keydown', (e) => e, false)
+     }
+  },[])
   return (
     <>
       <Nav />
       <main className='max-w-[800px] mx-auto mt-4 p-4'>
+        <CommandPalette/>
         <AboutMe />
         <LastProjects data={data} />
         <FrequentlyTechs />
